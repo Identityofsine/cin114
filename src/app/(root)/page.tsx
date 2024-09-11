6ff672856872934f133a04d9ea28a21f6de1254f
Kevin <identityofsine@gmail.com>
2024-09-11 19:04:14 -0400

    adding grid stuff

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,6 +2,42 @@ import CinLogo from '@/components/CinLogo';
 import '../styles/home.scss'
 import { VideoFeature } from '@/components/home/VideoFeature';
 
+type VideoMetadata = {
+	title: string;
+	url: string;
+	date: string;
+	img?: string;
+}
+
+const DUMMY_VIDEOS: VideoMetadata[][] = [[
+	{
+		title: 'Running From',
+		url: 'https://www.youtube.com/watch?v=0fKg7e37bQE',
+		date: '12/06/2023',
+		img: '/film/runningfrom/runningfrom.jpg'
+	}
+],
+[
+	{
+		title: 'Running From',
+		url: 'https://www.youtube.com/watch?v=0fKg7e37bQE',
+		date: '12/06/2023',
+		img: '/film/runningfrom/runningfrom2.jpg'
+	},
+]
+]
+
+function repeat(n: number) {
+	const arr = [];
+	let z = 0;
+	for (let i = 0; i < n; i++) {
+		if (z === DUMMY_VIDEOS.length) z = 0;
+		arr.push(DUMMY_VIDEOS[z][0]);
+		z++;
+	}
+	return arr;
+}
+
 export default function Home() {
 	return (
 		<section className="home">
@@ -9,7 +45,9 @@ export default function Home() {
 				<CinLogo />
 			</div>
 			<div className="home__content">
-				<VideoFeature />
+				<VideoFeature videos={repeat(5)} style={'horiziontal'} title={'Cinema'} />
+				<VideoFeature videos={repeat(4)} style={'mix'} title={'Videography'} />
+				<VideoFeature videos={repeat(3)} style={'vertical'} title={'Music Videos'} />
 			</div>
 		</section>
 	);
--- src/components/home/VideoFeature.tsx
+++ src/components/home/VideoFeature.tsx
@@ -1,35 +1,50 @@
-import { Postpone } from 'next/dist/server/app-render/dynamic-rendering';
+"use client";
+
 import '../styles/videofeature.scss';
+import { useRef } from 'react';
 
 
-const DUMMY_VIDEOS: VideoMetadata[][] = [[
-	{
-		title: 'Running From',
-		url: 'https://www.youtube.com/watch?v=0fKg7e37bQE',
-		date: '12/06/2023',
-		img: '/film/runningfrom/runningfrom.jpg'
+
+
+type VideoFeatureProp = {
+	title: string
+	style: 'horiziontal' | 'vertical' | 'mix'
+	videos: VideoMetadata[]
+}
+
+
+
+
+export function VideoFeature({ title, style, videos }: VideoFeatureProp) {
+
+	const maxVideos = useRef(videos.length > 5 ? 5 : videos.length);
+
+	function renderVideos() {
+		if (style === 'horiziontal') {
+			return (
+				<>
+					<VideoShowcase video={videos.slice(0, 1)} style={style} />
+					{maxVideos.current > 1 && <VideoShowcase video={videos.slice(1, 3)} style={style} />}
+					{(maxVideos.current > 3 && <VideoShowcase video={videos.slice(3, 5)} style={style} altgrow />)}
+				</>
+			)
+		} else {
+			return (
+				<>
+					{videos.map((video, index) => (
+						<VideoShowcase key={video.url + index} className={`cvideo-${index}`} video={[video]} style={style} />
+					))}
+				</>
+			)
+		}
 	}
-],
-[
-	{
-		title: 'Running From',
-		url: 'https://www.youtube.com/watch?v=0fKg7e37bQE',
-		date: '12/06/2023',
-		img: '/film/runningfrom/runningfrom2.jpg'
-	},
-]
-]
 
-
-export function VideoFeature() {
 	return (
 		<div className="video-feature">
-			<h2>Cinema</h2>
+			<h2>{title}</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<div className="video-feature__videos">
-				<VideoShowcase video={DUMMY_VIDEOS[0]} />
-				<VideoShowcase video={[...DUMMY_VIDEOS[0], ...DUMMY_VIDEOS[1]]} />
-				<VideoShowcase video={[...DUMMY_VIDEOS[1], ...DUMMY_VIDEOS[0]]} altgrow />
+			<div className={`video-feature__videos video-feature__videos__${style}`}>
+				{renderVideos()}
 			</div>
 		</div>
 	);
@@ -44,10 +59,12 @@ type VideoMetadata = {
 
 type VideoShowcaseProps = {
 	video: VideoMetadata[];
+	style: 'horiziontal' | 'vertical' | 'mix'
+	className?: string
 	altgrow?: boolean
 }
 
-function VideoShowcase({ video, altgrow = false }: VideoShowcaseProps) {
+function VideoShowcase({ video, style, className = "", altgrow = false }: VideoShowcaseProps) {
 
 	if (video.length === 0) throw new Error('No videos provided');
 
@@ -55,9 +72,9 @@ function VideoShowcase({ video, altgrow = false }: VideoShowcaseProps) {
 		console.warn('Only the first 2 videos will be displayed');
 	} else if (video.length <= 2) {
 		return (
-			<div className="videos-showcase">
+			<div className={`videos-showcase videos-showcase-${style} ${className}`}>
 				{video.map((video, index) => (
-					<div className={`video-showcase video-${index + 1} ${index > 0 && altgrow && 'altgrow'}`} style={{ backgroundImage: `url('${video.img}')` }}>
+					<div key={video?.date + index} className={`video-showcase video-${style} video-${index + 1} ${index > 0 && altgrow && 'altgrow'}`} style={{ backgroundImage: `url('${video.img}')` }}>
 						<div className="flex column video-showcase__title">
 							<h3>{video.title}</h3>
 							<span>{video.date}</span>
