695aaf3723014a9b7620cae9fb20ccf877cad231
identityofsine <identityofsine@gmail.com>
2024-09-19 16:04:44 -0400

    added 4040

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import './styles/notfound.scss';
+
+export default function NotFound() {
+	return (
+		<section className="notfound">
+			<h2>Not Found</h2>
+			<p>Could not find requested resource</p>
+		</section>
+	)
+}
