16821f57a5704d89d4ecc37caa323e12b41ab1ed
identityofsine <identityofsine@gmail.com>
2025-04-26 15:47:54 -0400

    displaying build date?

--- src/services/build.ts
+++ src/services/build.ts
@@ -8,8 +8,10 @@ export interface BuildInfo {
 
 export function getBuildInfo(): BuildInfo {
 	//this needs to be set in the build pipeline (e.g. GitHub Actions, etc.)
-	const buildId = process.env.BUILD_ID || 'head';
-	const buildDate = displayDate(new Date(process.env.BUILD_DATE || Date.now()));
+	const buildId = process.env.BUILD_ID || '';
+	const buildDate = displayDate(
+		new Date(process.env.BUILD_DATE || Date.now())
+	)
 	const branch = process.env.NEXT_PUBLIC_BRANCH_NAME === 'main' ? 'prod' : 'dev';
 
 	return {
