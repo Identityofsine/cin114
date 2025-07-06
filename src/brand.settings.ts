6d89afd49e1abafcd132ceac5a4bc3d99bb8bcd3
identityofsine <identityofsine@gmail.com>
2025-07-06 13:11:43 -0400

    whitespace

--- src/brand.settings.ts
+++ src/brand.settings.ts
@@ -1,18 +1,18 @@
 export const BrandSettings = {
-	url: 'https://cin114.net',
-	brandName: {
-		literal: 'cin114',
-		displayShort: 'CIN114',
-		display: 'Cinema-114'
-	},
-	contact: {
-		email: 'contact@cin114.net'
-	},
-	socials: {
-		youtube: 'https://www.youtube.com/@CIN114',
-		instagram: 'https://www.instagram.com/cin114films',
-		twitter: 'https://x.com/CIN114films',
-		pateron: 'https://www.patreon.com/CIN114',
-		vimeo: 'https://vimeo.com/user223422993'
-	}
+  url: 'https://cin114.net',
+  brandName: {
+    literal: 'cin114',
+    displayShort: 'CIN114',
+    display: 'Cinema-114'
+  },
+  contact: {
+    email: 'contact@cin114.net'
+  },
+  socials: {
+    youtube: 'https://www.youtube.com/@CIN114',
+    instagram: 'https://www.instagram.com/cin114films',
+    twitter: 'https://x.com/CIN114films',
+    pateron: 'https://www.patreon.com/CIN114',
+    vimeo: 'https://vimeo.com/user223422993'
+  }
 }
