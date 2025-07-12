c45143c58f01629ecb22b70158c92797e99ce54e
identityofsine <identityofsine@gmail.com>
2025-07-12 15:14:42 -0400

    added a lot of styling and general db health

--- src/components/screening/ScreeningHeader.tsx
+++ src/components/screening/ScreeningHeader.tsx
@@ -1,7 +1,12 @@
+import './styles/screeningheader.scss';
+
 function ScreeningHeader() {
   return (
-    <div></div>
-  )
+    <div className="screening-header">
+      <img src="/ui/cin114.svg" alt="CIN114" className="screening-header__side" />
+      <span className="screening-header__main">SCREENING</span>
+    </div>
+  );
 }
 
 export default ScreeningHeader
