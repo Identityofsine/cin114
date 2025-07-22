134f8b5e36dd3fe79f1a74c6aaff24b3086d7fee
identityofsine <identityofsine@gmail.com>
2025-07-22 19:56:51 -0400

    fixed compilation issues

--- src/components/screening/ScreeningMapPreview.tsx
+++ src/components/screening/ScreeningMapPreview.tsx
@@ -1,8 +1,22 @@
 'use client'
 import { EventLocation } from '@/types/event';
 import './styles/screeningmappreview.scss'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, MapContainerProps, TileLayer, TileLayerProps, Marker } from 'react-leaflet';
 
+interface MapContainerPropsFixed extends MapContainerProps {
+  center: [number, number];
+  zoom: number;
+  scrollWheelZoom?: boolean;
+}
+
+const MapContainerWrapper = MapContainer as unknown as (props: MapContainerPropsFixed) => JSX.Element;
+
+interface TileLayerPropsFixed extends TileLayerProps {
+  url: string;
+  attribution: string;
+}
+
+const TileLayerWrapper = TileLayer as unknown as (props: TileLayerPropsFixed) => JSX.Element;
 
 type ScreeningMapProps = {
   location?: EventLocation
@@ -24,14 +38,14 @@ function ScreeningMapPreview({ location }: ScreeningMapProps) {
 
   return (
     <div className="screening-map-preview">
-      <MapContainer className="screening-map-preview__map" center={[longitude, latitude]} zoom={24} scrollWheelZoom={false}>
-        <TileLayer
+      <MapContainerWrapper className="screening-map-preview__map" center={[longitude, latitude]} zoom={24} scrollWheelZoom={false}>
+        <TileLayerWrapper
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={[longitude, latitude]} />
 
-      </MapContainer>
+      </MapContainerWrapper>
     </div>
   )
 }
